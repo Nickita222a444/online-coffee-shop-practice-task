23a2d8c8252de2ffd742a4e8c953d6ef27b61b25
Nickita222a444 <nickitamalushev2016@yandex.ru>
2024-07-04 00:09:31 +0400

    Implemented the adaptive design for What-Our-Customers-Say section (>340px)

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -11,7 +11,10 @@ import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.brow
 const swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   loop: true,
-  slidesPerView: 2,
+  breakpoints: {
+    0: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
